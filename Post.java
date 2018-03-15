f27703e765da14b26cd48f9b34764635b2f12e4a
Zach Zimmerman <zzimmerman-student@nfcs.k12.pa.us>
2018-03-15 13:46:55 -0400

    Add files via upload

---
+++ Post.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+/**
+ * Write a description of class post here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Post
+{
+    
+    //change user
+    public static void changeUser(){
+        
+    }
+    
+    //remove
+    public static void removePost(){
+        
+    }
+    
+    //add
+    public static void addPost(){
+        
+    }
+    
+    public static void quit(){
+        
+    }
+    
+    public static void setUp(){
+        System.out.print("*Awesome animation*\nWelcome to the app.");
+    }
+}
